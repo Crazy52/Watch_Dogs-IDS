14deb93b7b121d756f44d7926a23b765be1f0d84
Crazy52 <gameby420@gmail.com>
2014-06-07 19:38:23 -0700

    First change scan.sh
    added scan.sh
    getting started

---
+++ scan.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+clear
+echo .____________________________.
+echo ! Intrusion Detection System !
+echo !----------------------------!
+ip=$(curl -s checkip.dyndns.org|sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
+echo "IP:" $ip
+echo Listing Availible Interfaces
+echo ----
+ifconfig -a | sed 's/[ \t].*//;/^$/d'
+echo ----
+read -p "Select an Interface [eth0]: " iface
+iface=${iface:-eth0}
+read -p "Save to File (y/n)?" save
+case "$save" in
+  y|Y ) write="-w " && file=$(date +%g%m%d_%H%M%S.pcap) && touch $file && echo Writing to $file;;
+  * ) write="" && file="" && echo "Writing Disabled";;
+esac
+echo Starting IDS on $iface
+tshark -i $iface -n -P -f "udp src port 3658 and udp dst port not 11005 and src host $ip" $write $file
